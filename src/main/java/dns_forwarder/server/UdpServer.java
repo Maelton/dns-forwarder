1b8764d84d41517d67c2baa1f4945e4a8e1db0f6
Filipe Zaidan <felipezaidan10@gmail.com>
2024-07-13 21:03:49 -0300

    feat: save response in cache

--- src/main/java/dns_forwarder/server/UdpServer.java
+++ src/main/java/dns_forwarder/server/UdpServer.java
@@ -1,12 +1,16 @@
 package dns_forwarder.server;
 
+import dns_forwarder.cache.CacheEntry;
 import dns_forwarder.client.ClientApplication;
 import dns_forwarder.datagram.DatagramDeserializer;
+import dns_forwarder.datagram.DatagramQuestionSectionDeserializer;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class UdpServer {
 
@@ -15,17 +19,22 @@ public class UdpServer {
     private DatagramSocket socket;
     private DatagramSocket forwardingSocket;
 
+    private int TTL = 5;
     private String GOOGLE_DNS_ADDRESS = "8.8.8.8";
     private int GOOGLE_DNS_PORT = 53;
 
     private ClientApplication clientHost;
 
+     private final ConcurrentHashMap<String, CacheEntry> cache;
+
     public UdpServer() {
         port = DEFAULT_PORT;
+        cache = new ConcurrentHashMap<>();
     }
 
     public UdpServer(int port) {
         this.port = port;
+        cache = new ConcurrentHashMap<>();
     }
 
     public void createDefaultSocket() {
@@ -47,12 +56,12 @@ public class UdpServer {
     }
 
     public void createServerSocket() {
-        if(port == DEFAULT_PORT)
+        if (port == DEFAULT_PORT) {
             createDefaultSocket();
-        else {
+        } else {
             try {
                 createCustomSocket(this.port);
-            } catch(DatagramSocketException e) {
+            } catch (DatagramSocketException e) {
                 System.out.println(e);
                 createDefaultSocket();
             }
@@ -60,7 +69,7 @@ public class UdpServer {
     }
 
     public void closeServerSocket() {
-        if(socket != null && !socket.isClosed()) {
+        if (socket != null && !socket.isClosed()) {
             socket.close();
             System.out.println("Server has stopped!");
         }
@@ -70,13 +79,13 @@ public class UdpServer {
         createServerSocket();
         createForwardingSocket();
 
-        if(socket != null & forwardingSocket != null) {
-            while(true) {
+        if (socket != null & forwardingSocket != null) {
+            while (true) {
                 DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
                 try {
                     socket.receive(packet);
                     forwardPacket(packet);
-                } catch(Exception e) {
+                } catch (Exception e) {
                     System.out.println("Could not receive packet: " + e.getMessage());
                 }
             }
@@ -92,13 +101,25 @@ public class UdpServer {
     }
 
     public void closeForwardingSocket() {
-        if(forwardingSocket != null && !forwardingSocket.isClosed())
+        if (forwardingSocket != null && !forwardingSocket.isClosed()) {
             forwardingSocket.close();
+        }
     }
 
-    public void forwardPacket(DatagramPacket receivedPacket) {
-        DatagramPacket packet = null;
+    public void forwardPacket(DatagramPacket receivedPacket) throws IOException {
+        DatagramQuestionSectionDeserializer deserializer = new DatagramQuestionSectionDeserializer(receivedPacket);
+        String query = deserializer.getQNAME();
 
+        CacheEntry cachedResponse = cache.get(query);
+        if (cachedResponse != null && !cachedResponse.isExpired()) {
+            System.out.println("tem cache");
+
+            socket.send(new DatagramPacket(cachedResponse.getData(), cachedResponse.getData().length, receivedPacket.getAddress(), receivedPacket.getPort()));
+            return;
+        }
+
+        System.out.println("n tem cache");
+        DatagramPacket packet = null;
         clientHost = new ClientApplication(receivedPacket.getPort(), receivedPacket.getAddress());
         try {
             packet = new DatagramPacket(receivedPacket.getData(),
@@ -109,24 +130,33 @@ public class UdpServer {
         }
 
         try {
-            if(packet != null) {
+            if (packet != null) {
+
                 forwardingSocket.send(packet);
                 System.out.println("Forwarded packet to " + GOOGLE_DNS_ADDRESS + ":" + GOOGLE_DNS_PORT);
 
                 DatagramPacket responsePacket = new DatagramPacket(new byte[1024], 1024);
                 forwardingSocket.receive(responsePacket);
                 System.out.println(new DatagramDeserializer(responsePacket));
+
+                // Armazenar resposta no cache
+
+                cache.put(query, new CacheEntry(responsePacket.getData(), TTL));
                 sendResponseClient(responsePacket);
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("Could not forward packet: " + e.getMessage());
         }
     }
 
     public void sendResponseClient(DatagramPacket responseForwarderPacket) throws IOException {
         DatagramPacket responseClientPacket = new DatagramPacket(responseForwarderPacket.getData(),
-                                        responseForwarderPacket.getLength(),
-                                        clientHost.getIp(), clientHost.getPort());
+                responseForwarderPacket.getLength(),
+                clientHost.getIp(), clientHost.getPort());
         socket.send(responseClientPacket);
+    }
+
+    private long extractTTL(byte[] data) {
+        return 10;
     }
 }
